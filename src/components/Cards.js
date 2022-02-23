d108825c8b180269e024fa48b2bf64d0e2b0c8e2
jeremie thery <thery.jeremie@yahoo.fr>
2022-02-23 23:01:23 +0100

    Recuperation de l api via axio

---
+++ src/components/Cards.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const cards = (props) => {
+    const {film} = props
+    console.log(film)
+    return (
+        <div>  
+        Card
+        </div>
+    );
+};
+
+export default cards;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,19 +1,27 @@
 import React ,{useState,useEffect}from 'react';
 import axios from "axios";
+import Card from './Cards';
 
 const Movie = () => {
     const [data,setData] = useState([]);
+
     useEffect(()=>{
         axios
         .get(
             "https://api.themoviedb.org/3/discover/movie?api_key=6c96d79bf66aed241beb96eff283df85" 
         )
-        .then((res)=>setData(res.data));
-        console.log(data)
+        .then((res)=>setData(res.data.results));
+       console.log(data);
+    
     },[]);
-
+   
     return (
-        <div>toto</div>
+        <div><ul>
+            {data.map((film)=>(
+                <Card film={film} key={film.title}/>
+            ))}
+            
+            </ul></div>
     );
 };
 
