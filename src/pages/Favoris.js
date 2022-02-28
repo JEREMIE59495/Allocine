f522ee329bc1eb1f57d0c441ffc931bc6b3c1286
jeremie thery <thery.jeremie@yahoo.fr>
2022-02-28 20:53:24 +0100

    correction erreur sur lapage favoris

--- src/pages/Favoris.js
+++ src/pages/Favoris.js
@@ -4,31 +4,29 @@ const Favoris = () => {
     const favoris = localStorage.getItem('favoris')
     console.log(favoris)
     let tabFavoris =[];
-    if(favoris !=null){
+    if(!!favoris ){
         let splitStorage = favoris.split(',')
-        //TODO : Gérer l'erreur de la console :(Warning: Each child in a list should have a unique "key" prop.)
-        
+  
         for(let i=0;i<splitStorage.length;i++){
             let splitTab =splitStorage[i].split(';')
             tabFavoris.push(splitTab) } 
+           
     }
-    
-    console.log(tabFavoris)
-    return (
-        <div>
-            
-            <h1>Mes favoris</h1>
-            <ul>
-                
-                {tabFavoris.map((fav)=>(
-                    <div className='div_favoris'>
-                    <img className="favoris_img"src={`https://image.tmdb.org/t/p/original${fav[1]}`} alt={`affiche ${fav[0]}`}/>
-                    <h2 className='title_favoris'>{fav[0]}</h2>
-                    </div>
-                ))}
-            </ul>
-        </div>
-    );
+     return (
+                <div>
+                    
+                    <h1>Mes favoris</h1>
+                    <ul >   
+                        {tabFavoris.map((fav)=>(
+                            <div className='div_favoris'>
+                            <img className="favoris_img"src={`https://image.tmdb.org/t/p/original${fav[1]}`} alt={`affiche ${fav[0]}`}/>
+                            <h2 className='title_favoris'>{fav[0]}</h2>
+                            </div>
+                        ))}
+                    </ul>
+                </div>
+            );
+ 
 };
 
 export default Favoris;
